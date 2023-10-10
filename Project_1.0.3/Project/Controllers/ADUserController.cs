8613461cc0b3f23a2cdbe53caacf58755b315cb4
SoongKim <127727195+SoongKim@users.noreply.github.com>
2023-10-10 17:09:39 +0900

    2023.10.10.
    Csharp Project. Still Progressing.

---
+++ Project_1.0.3/Project/Controllers/ADUserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.DirectoryServices;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class ADUserController : ControllerBase
+    {
+        [HttpPost("usercreate")]
+        public IActionResult UserCreate([FromBody] AddsUserInsertModel addsM)
+        {
+            try
+            {
+                using (DirectoryEntry entry = new DirectoryEntry(string.Format("LDAP://OU={0},OU=Group,DC=kopoproject,DC=dev", addsM.userOu)))
+                {
+                    DirectoryEntries childEntries = entry.Children;
+                    DirectoryEntry newUser = childEntries.Add("CN=" + addsM.userName, "user");
+                    newUser.Properties["samAccountName"].Value = addsM.userId;
+                    newUser.CommitChanges();
+
+                    // 사용자를 그룹에 추가
+                    DirectoryEntry groupEntry = new DirectoryEntry(string.Format("LDAP://CN=Development,OU=Group,DC=kopoproject,DC=dev", addsM.userOu));
+                    groupEntry.Properties["member"].Add(newUser.Properties["distinguishedName"].Value);
+                    groupEntry.CommitChanges();
+                    return Ok("생성이 완료되었습니다.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Project_1.0.3/README.txt.txt
@@ -0,0 +1,9 @@
+2023.10.10.
+
+Services.QueryServices.Insert 파트 오류 수정
+
+Controllers.dbWork.cs LogSession 파트 오류 수정
+
+Active Directory - 지정한 조직 구성 단위에 신규 유저를 생성하는 Api Controller Endpoint 신설
+
+=> 추후 소속 조직 구성 단위의 수정 및 권한 회수, 유저 삭제 등의 추가 기능 구현 예정
